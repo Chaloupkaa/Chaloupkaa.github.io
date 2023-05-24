b116f6f7075173befa7cd85a9a036e3faa5f83f3
Chaloupkaa <127877322+Chaloupkaa@users.noreply.github.com>
2023-05-24 16:47:22 +0200

    Add files via upload

---
+++ mainPage.js
@@ -0,0 +1,65 @@
+const _0x259645 = _0x2111;
+(function (_0x336cb0, _0x81818) {
+  const _0x489f53 = _0x2111,
+    _0x589138 = _0x336cb0();
+  while (!![]) {
+    try {
+      const _0x5664c6 =
+        parseInt(_0x489f53(0x1bd)) / 0x1 +
+        parseInt(_0x489f53(0x1c1)) / 0x2 +
+        parseInt(_0x489f53(0x1ba)) / 0x3 +
+        -parseInt(_0x489f53(0x1c0)) / 0x4 +
+        (parseInt(_0x489f53(0x1c3)) / 0x5) * (-parseInt(_0x489f53(0x1bc)) / 0x6) +
+        parseInt(_0x489f53(0x1c5)) / 0x7 +
+        -parseInt(_0x489f53(0x1bf)) / 0x8;
+      if (_0x5664c6 === _0x81818) break;
+      else _0x589138["push"](_0x589138["shift"]());
+    } catch (_0x1aa53c) {
+      _0x589138["push"](_0x589138["shift"]());
+    }
+  }
+})(_0x46e8, 0x2bd87);
+const diceGame = document[_0x259645(0x1c4)](_0x259645(0x1be)),
+  minesGame = document[_0x259645(0x1c4)](_0x259645(0x1b8));
+function _0x2111(_0xd90ec4, _0x2afe6c) {
+  const _0x46e86f = _0x46e8();
+  return (
+    (_0x2111 = function (_0x21119f, _0x585f54) {
+      _0x21119f = _0x21119f - 0x1b8;
+      let _0x3d5952 = _0x46e86f[_0x21119f];
+      return _0x3d5952;
+    }),
+    _0x2111(_0xd90ec4, _0x2afe6c)
+  );
+}
+diceGame[_0x259645(0x1b9)](_0x259645(0x1c2), () => {
+  const _0x1d5f8c = _0x259645;
+  window["location"][_0x1d5f8c(0x1bb)] = "dice/dice.html";
+}),
+  minesGame["addEventListener"](_0x259645(0x1c2), () => {
+    const _0x1b22c3 = _0x259645;
+    window[_0x1b22c3(0x1c6)][_0x1b22c3(0x1bb)] = "mines/mines.html";
+  });
+function _0x46e8() {
+  const _0x381651 = [
+    "835VuZWjd",
+    "getElementById",
+    "1450939XgcWLJ",
+    "location",
+    "minesGame",
+    "addEventListener",
+    "579171yfpNIY",
+    "href",
+    "8058HoZRKZ",
+    "208907ogLspy",
+    "diceGame",
+    "2344448DnCyBZ",
+    "294524RaZVhu",
+    "322636mhbMKE",
+    "click",
+  ];
+  _0x46e8 = function () {
+    return _0x381651;
+  };
+  return _0x46e8();
+}
